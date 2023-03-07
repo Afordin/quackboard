a4b5ab2778cdd48e6a6371b8926782086151d51c
adriDiazz <adriandiazmanzanares9@gmail.com>
2023-03-07 13:52:59 +0100

    Margin fixed

--- pages/songs/index.tsx
+++ pages/songs/index.tsx
@@ -1,29 +1,27 @@
-import { useEffect, useState } from 'react'
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
-import SongRow from '@/components/SongRow'
-import type { Song } from '@/types'
+import { useEffect, useState } from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import SongRow from "@/components/SongRow";
+import type { Song } from "@/types";
 
 export default function List() {
-  const supabase = useSupabaseClient()
+  const supabase = useSupabaseClient();
   // const [playing, setPlaying] = useState(false);
-  const [songs, setSongs] = useState<Song[] | null>(null)
+  const [songs, setSongs] = useState<Song[] | null>(null);
   const getSongsFromSupabase = async () => {
-    const { data, error } = await supabase.from('canciones').select('*')
-    error
-      ? console.log(error)
-      : setSongs(data as Song[])
-  }
+    const { data, error } = await supabase.from("canciones").select("*");
+    error ? console.log(error) : setSongs(data as Song[]);
+  };
 
   useEffect(() => {
-    getSongsFromSupabase()
-  }, [])
+    getSongsFromSupabase();
+  }, []);
 
   return (
     <section>
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         Lista de canciones
       </h2>
-      <ul role="list" className="grid grid-cols-2 gap-4">
+      <ul role="list" className="grid grid-cols-2 gap-4 mt-6">
         {songs?.map((song) => {
           return (
             <li key={song.id.toString()}>
@@ -31,9 +29,9 @@ export default function List() {
                 <SongRow song={song} />
               </article>
             </li>
-          )
+          );
         })}
       </ul>
     </section>
-  )
+  );
 }
