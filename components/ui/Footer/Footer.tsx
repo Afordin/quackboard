fbebf8a4c2f28142f6bea145989f5ec9f4f36bac
Unai Mengual <git@owln.ai>
2023-03-08 19:52:23 +0100

    chore: footer improvements

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center px-6 py-2 text-sm font-medium bg-white border-2 border-black rounded-full md:justify-between md:gap-0 md:flex-row">
-      <div className="comic-text relevance">(c) 2023 Cuack cuack</div>
+      <div className="comic-text relevance">
+        <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="hover:underline">
+          © 2023 Cuack cuack
+        </Link></div>
       <div className="text-black">Ningún pato fue dañado durante el desarrollo de la app</div>
-      <div className="comic-text relevance">#Hackafor2023</div>
+      <div className="comic-text relevance">
+        <Link href="https://github.com/owlnai/quackboard" className="hover:underline">
+          #Hackafor2023
+        </Link>
+      </div>
     </footer>
   )
 }
